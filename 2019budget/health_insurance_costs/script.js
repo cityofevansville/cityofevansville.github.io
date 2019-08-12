bc7e228e3beb2dbfdadf1f8e904467467291fb42
Noah Stubbs <noah.stubbs@gmail.com>
2019-08-12 16:46:16 -0500

    Update script.js

--- 2019budget/health_insurance_costs/script.js
+++ 2019budget/health_insurance_costs/script.js
@@ -31,7 +31,6 @@
   ]
 };
     var options = {
-        scaleLabel: function(label){return  '$' + label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
   "title": {
     "display": true,
     "text": "Health Insurance Costs"
